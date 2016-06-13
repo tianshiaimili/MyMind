1e819d2667aef9ef339246f287452be92472d0d8
tianshiaimili <913540483@qq.com>
2016-06-13 16:49:54 +0800

    生鲜品类卖家管理规范

---
+++ 生鲜品类卖家管理规范.mm
@@ -0,0 +1,29 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1465807233100" ID="ID_1139487251" MODIFIED="1465807525293">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b><font size="5">&#29983;&#40092;&#21697;&#31867;&#21334;&#23478;&#31649;&#29702;&#35268;&#33539;</font></b>
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1465807507962" ID="ID_499571477" MODIFIED="1465807547558" POSITION="right" TEXT="&#x597d;&#x8bc4;&#x7387;&#x7ba1;&#x7406;&#x7ec6;&#x5219;&#xff1a;">
+<node CREATED="1465807549594" ID="ID_545803497" MODIFIED="1465807558178" TEXT="&#x5546;&#x54c1;&#xff08;SKU&#xff09;&#x603b;&#x8bc4;&#x4ef7;&#x6570;&#x2265;20&#x6761;&#xff0c;&#x597d;&#x8bc4;&#x7387;&#x2264;84%">
+<node CREATED="1465807603118" ID="ID_1876462127" MODIFIED="1465807604633" TEXT="&#x5bf9;&#x8be5;&#x5546;&#x54c1;SKU&#x8fdb;&#x884c;&#x4e0b;&#x67b6;&#x5904;&#x7406;"/>
+</node>
+<node CREATED="1465807559888" ID="ID_1466659399" MODIFIED="1465807568618" TEXT=" &#x5546;&#x54c1;&#xff08;SKU&#xff09;&#x603b;&#x8bc4;&#x4ef7;&#x6570;&#xff1c;20&#x6761;&#xff0c;&#x597d;&#x8bc4;&#x7387;&#x2264;84%">
+<node CREATED="1465807619238" ID="ID_446827675" MODIFIED="1465807620533" TEXT="&#x4eac;&#x4e1c;&#x5e73;&#x53f0;&#x53d1;&#x51fa;&#x8b66;&#x544a;&#x6574;&#x6539;&#x901a;&#x77e5;&#xff0c;&#x6574;&#x6539;&#x671f;&#x9650;&#x4e3a;&#x4e00;&#x5468;"/>
+</node>
+</node>
+<node CREATED="1465807665560" ID="ID_1117593370" MODIFIED="1465807667831" POSITION="left" TEXT="&#x549a;&#x549a;&#x54cd;&#x5e94;&#xff08;&#x4e00;&#x822c;&#x8fdd;&#x89c4;&#xff09;">
+<node CREATED="1465807669821" ID="ID_1150172385" MODIFIED="1465807682184" TEXT="&#x5728;&#x7ebf;&#x670d;&#x52a1;&#x65f6;&#x957f;&#xff1a;&#x5546;&#x5bb6;&#x5e94;&#x4fdd;&#x8bc1;&#x671f;&#x5ba2;&#x670d;&#x56e2;&#x961f;&#xff08;&#x6216;&#x4eba;&#x5458;&#xff09;&#x6bcf;&#x5468;&#x4e00;&#x81f3;&#x5468;&#x65e5;9:00-18:00&#x5904;&#x4e8e;&#x549a;&#x549a;&#x5728;&#x7ebf;&#x72b6;&#x6001;&#xff0c;&#x5e97;&#x94fa;&#x549a;&#x549a;&#x5728;&#x7ebf;&#x670d;&#x52a1;&#x65f6;&#x957f;&#x4e0d;&#x5f97;&#x5c11;&#x4e8e;9&#x5c0f;&#x65f6;"/>
+<node CREATED="1465807722439" ID="ID_50958267" MODIFIED="1465807724382" TEXT="&#x5e94;&#x7b54;&#x7387;&#xff1a;&#x5e97;&#x94fa;&#x5468;&#x5e73;&#x5747;&#x6bcf;&#x65e5;&#x54cd;&#x5e94;&#x7528;&#x6237;&#x7684;&#x65f6;&#x95f4;&#x5e94;&#x4fdd;&#x6301;&#x5728;40&#x79d2;&#x5185;"/>
+<node CREATED="1465807726251" ID="ID_1521748626" MODIFIED="1465807745863" TEXT="&#x5356;&#x5bb6;&#x549a;&#x549a;&#x6ee1;&#x610f;&#x5ea6;&#x5468;&#x6307;&#x6807;&#x5e94;&#x4e0d;&#x4f4e;&#x4e8e;80% "/>
+</node>
+</node>
+</map>
