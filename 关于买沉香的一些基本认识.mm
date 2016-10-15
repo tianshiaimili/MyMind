47baaa8638efb7ef41f67832725d51c8f05c22b2
tianshiaimili <913540483@qq.com>
2016-10-15 23:44:27 +0800

    555

---
+++ 关于买沉香的一些基本认识.mm
@@ -0,0 +1,19 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1476544833186" ID="ID_1227339492" MODIFIED="1476544852795" TEXT="&#x5173;&#x4e8e;&#x4e70;&#x6c89;&#x9999;&#x7684;&#x4e00;&#x4e9b;&#x57fa;&#x672c;&#x8ba4;&#x8bc6;">
+<node CREATED="1476544957593" ID="ID_299334930" MODIFIED="1476544977671" POSITION="right" TEXT="&#x7f51;&#x4e0a;&#x7684;&#x4e00;&#x4e9b;&#x8fa8;&#x522b;&#x65b9;&#x6cd5;">
+<node CREATED="1476544979117" ID="ID_738450697" MODIFIED="1476544979830" TEXT="&#x751f;&#x95fb;&#x5373;&#x9999;&#x98d8;&#x56db;&#x6ea2;&#x7684;&#xff0c;&#x8bf7;&#x52ff;&#x8d2d;&#x4e70;&#xff01;"/>
+<node CREATED="1476545036709" ID="ID_723912144" MODIFIED="1476545037462" TEXT="&#x71c3;&#x70e7;&#x65f6;&#x95f4;&#x8fc7;&#x957f;&#xff0c;&#x8bf7;&#x64e6;&#x4eae;&#x773c;&#x775b;&#xff01;"/>
+<node CREATED="1476545049491" ID="ID_1062702026" MODIFIED="1476545050735" TEXT="&#x9999;&#x7070;&#x4e0d;&#x65ad;&#xff0c;&#x8bf7;&#x518d;&#x770b;&#x4ed4;&#x7ec6;&#xff01;">
+<node CREATED="1476545106667" ID="ID_1216623926" MODIFIED="1476545116341" TEXT="&#x6709;&#x53ef;&#x80fd;&#x6dfb;&#x52a0;&#x5316;&#x5b66;&#x56fa;&#x5316;&#x5242;&#xff0c;&#x7c98;&#x5408;&#x5242;&#x6216;&#x8005;&#x9676;&#x571f;"/>
+<node CREATED="1476545117308" ID="ID_158041503" MODIFIED="1476545131116" TEXT="&#x542b;&#x6cb9;&#x91cf;&#x548c;&#x5de5;&#x827a;"/>
+</node>
+<node CREATED="1476545192764" ID="ID_20543906" MODIFIED="1476545194001" TEXT="&#x9999;&#x7070;&#x70eb;&#x624b;&#xff0c;&#x8bf7;&#x8fdc;&#x79bb;&#xff01;"/>
+<node CREATED="1476545381521" ID="ID_972267789" MODIFIED="1476545382988" TEXT="&#x5916;&#x89c2;&#x7ec6;&#x817b;&#x989c;&#x8272;&#x8273;&#x4e3d;&#xff0c;&#x8bf7;&#x653e;&#x5f03;&#xff01;"/>
+<node CREATED="1476545391330" ID="ID_496703702" MODIFIED="1476545392657" TEXT="&#x4ef7;&#x683c;&#x201c;&#x4eb2;&#x6c11;&#x201d;&#xff0c;&#x8bf7;&#x6ce8;&#x610f;&#xff01;"/>
+</node>
+<node CREATED="1476544982067" ID="ID_1682327783" MODIFIED="1476544986597" POSITION="left" TEXT="&#x53c2;&#x8003;">
+<node CREATED="1476544993689" ID="ID_1611413112" MODIFIED="1476544994958" TEXT="http://blog.tianya.cn/post-6382507-112126192-1.shtml"/>
+</node>
+</node>
+</map>
