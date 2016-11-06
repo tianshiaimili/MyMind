136497f4b0b4fc4a7b57a3bb77d6c7e7c3179863
tianshiaimili <913540483@qq.com>
2016-11-07 00:20:01 +0800

    qqqqq

---
+++ 产品标题-淘宝京东.mm
@@ -0,0 +1,15 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1478182595320" ID="ID_971274009" MODIFIED="1478182647185" TEXT="&#x4ea7;&#x54c1;&#x6807;&#x9898;">
+<node CREATED="1478182648689" ID="ID_1258859348" MODIFIED="1478182651593" POSITION="right" TEXT="&#x6dd8;&#x5b9d;">
+<node CREATED="1478182657637" ID="ID_1979449480" MODIFIED="1478182662067" TEXT="&#x5de5;&#x827a;&#x54c1;">
+<node CREATED="1478182663380" ID="ID_957957402" MODIFIED="1478182665653" TEXT="&#x7eb8;&#x5dfe;&#x76d2;">
+<node CREATED="1478182733847" ID="ID_1984739311" MODIFIED="1478182935271" TEXT="&#x7ea2;&#x6728;&#x6728;&#x8d28;&#x7eb8;&#x5dfe;&#x76d2; &#x5b9e;&#x6728;&#x62bd;&#x7eb8;&#x76d2;&#x9ad8;&#x6863;&#x6728;&#x96d5;&#x62bd;&#x7eb8;&#x7b52;&#x9910;&#x5dfe;&#x76d2;&#x7ea2;&#x6728;&#x5b9a;&#x5236;logo&#x82b1;&#x68a8;&#x6728;&#x5b9e;&#x6728;">
+<node CREATED="1478182941881" ID="ID_1005436495" MODIFIED="1478183713933" TEXT="&#x5e97;&#x94fa;&#x5728;&#x5e7f;&#x897f;&#x4e1c;&#x5174;&#x5e02;&#x3001;&#x4e00;&#x4e2a;&#x9760;&#x8fd1;&#x8d8a;&#x5357;&#x7684;&#x8fb9;&#x8d38;&#x57ce;&#x5e02;&#xff0c;&#x6728;&#x6750;&#x6765;&#x81ea;&#x8d8a;&#x5357;&#x7684;&#x6210;&#x5e74;&#x82b1;&#x68a8;&#x6728;&#xff0c;&#x8d28;&#x5730;&#x7ec6;&#x5bc6;&#x7ed3;&#x5b9e;&#x3001;&#x6728;&#x7eb9;&#x6e05;&#x6670;&#x53ef;&#x89c1;&#x3001;&#x82b1;&#x96d5;&#x7cbe;&#x81f4;&#x5927;&#x6c14;&#xff01;&#x5178;&#x96c5;&#x6697;&#x7ea2;&#x8272;&#x7684;&#x5916;&#x578b;&#x548c;&#x6728;&#x5236;&#x5bb6;&#x5177;&#x642d;&#x914d;&#x517b;&#x773c;&#xff0c;&#x5f70;&#x663e;&#x751f;&#x6d3b;&#x54c1;&#x5473;&#x3002;   &#x5b9e;&#x6728;&#x6750;&#x6599;&#x6bd4;&#x5851;&#x6599;&#x6750;&#x6599;&#x7528;&#x5f97;&#x66f4;&#x4e45;&#x66f4;&#x7701;&#x94b1;&#x3002;    &#x5b9d;&#x8d1d;&#x91c7;&#x7528;&#x4e9a;&#x5149;&#x6253;&#x78e8;&#x7684;&#x5236;&#x4f5c;&#x5de5;&#x827a;&#xff0c;&#x7eb8;&#x5dfe;&#x76d2;&#x7528;&#x6599;&#x539a;&#xff0c;&#x62ff;&#x8d77;&#x6765;&#x76f8;&#x5f53;&#x6c89;&#x624b;&#xff01;&#x7528;&#x6765;&#x88c5;&#x626e;&#x60a8;&#x7684;&#x5bb6;&#x7edd;&#x5bf9;&#x9ad8;&#x7aef;&#x3001;&#x5927;&#x6c14;&#x3001;&#x4e0a;&#x6863;&#x6b21;&#x3002;&#x597d;&#x6728;&#x6750;&#xff0c;&#x597d;&#x6837;&#x5f0f;&#xff0c;&#x597d;&#x505a;&#x5de5;&#xff01;&#x968f;&#x65f6;&#x90fd;&#x6709;&#x53ef;&#x80fd;&#x65ad;&#x8d27;&#x54e6;"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1478182652776" ID="ID_1205169227" MODIFIED="1478182655075" POSITION="right" TEXT="&#x4eac;&#x4e1c;"/>
+</node>
+</map>
